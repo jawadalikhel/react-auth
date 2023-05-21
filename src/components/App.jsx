5d5a967e72c59af5c8f818b81dee10dd3851bf12
jawadalikhel <jawadalikhel18@gmail.com>
2023-05-20 19:04:46 -0500

    creted a separate component for form

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
